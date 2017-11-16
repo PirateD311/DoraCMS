fb7e3ead4fea1696caa7b2aa390ac75b5ef83908
刘珣 <851652491@qq.com>
2017-11-16 16:05:48 +0800

    init picture

---
+++ src/picture/App.vue
@@ -0,0 +1,65 @@
+<style lang="scss">
+</style>
+<template>
+    <div id="app">
+        <MyHeader v-if="$route.meta.typeId != 'adminlogin'" />
+        <transition name="fade" mode="out-in">
+            <router-view :key="$route.fullPath" v-if="$route.meta.notKeepAlive" class="view"></router-view>
+        </transition>
+        <transition name="fade" mode="out-in">
+            <keep-alive>
+                <router-view :key="$route.fullPath" v-if="!$route.meta.notKeepAlive" class="view"></router-view>
+            </keep-alive>
+        </transition>
+        <h6 :sm='24' :md="0" style="margin: 0px 5px;padding: 5px 20px;text-align: center;border: 1px solid #4CAF50;">本站承诺不含任何违禁图片、文字或视频，如有发现请立即向管理员反馈删除!更多绅士福利+QQ:230714605</h6>
+        <MyFooter v-if="$route.meta.typeId != 'adminlogin'" />
+    </div>
+</template>
+<script>
+import {
+    mapGetters,
+    mapState
+} from 'vuex'
+
+import MyHeader from './components/header'
+import MyFooter from './components/Footer'
+
+export default {
+    name: 'app',
+    components: {
+        MyHeader,
+        MyFooter
+    },
+    data() {
+        return {}
+    },
+    computed: {
+        ...mapGetters({
+            global: 'global/getGlobal'
+        }),
+        ...mapState('appShell', [
+            'pageTransitionName'
+        ]),
+        key() {
+            return this.$route.path.replace(/\//g, '_')
+        },
+        backend() {
+            return this.$route.path.indexOf('backend') >= 0
+        },
+        isLogin() {
+            return this.$route.path === '/backend'
+        }
+    },
+    methods: {
+        handleBeforeEnter() {
+            this.$store.dispatch('appShell/setPageSwitching', true)
+        },
+        handleAfterEnter() {
+            this.$store.dispatch('appShell/setPageSwitching', false)
+        },
+        handleClickHeaderBack() {
+            this.$router.go(-1)
+        },
+    }
+}
+</script>
---
+++ src/picture/views/Article.vue
@@ -0,0 +1,190 @@
+<template>
+    <div>
+        <div class="content-detail">
+            <div class="readme">
+                <el-row :gutter="0" class="header-main">
+                    <el-col :xs="1" :sm="1" :md="3" :lg="3">
+                        <div class="grid-content bg-purple">&nbsp;</div>
+                    </el-col>
+                    <el-col :xs="22" :sm="22" :md="18" :lg="18">
+                        <el-row :gutter="24">
+                            <el-col :xs="24" :sm="17" :md="17" :lg="17">
+                                <div>
+                                    <h2 class="content-title">{{article.doc.title}}</h2>
+                                    <div class="content-author">
+                                        <ul>
+                                            <li class="author-name">
+                                                <el-tag size="mini">{{article.doc.author ? article.doc.author.name:''}}</el-tag>
+                                            </li>
+                                            <li>
+                                                <span class="dot">&nbsp;•&nbsp;</span>{{cateName}}
+                                            </li>
+                                            <li>
+                                                <span class="dot">&nbsp;•&nbsp;</span>{{article.doc.date}}
+                                            </li>
+                                            <li>
+                                                <span class="dot">&nbsp;•&nbsp;</span>{{article.doc.clickNum}}&nbsp;次阅读
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div v-if="!article.doc.isVip || loginState.logined" >
+                                        <div v-html="article.doc.comments"></div>
+                                    </div>
+                                    <div style="min-height:400px" v-else>
+                                        <h3 style="color:#fa5555">抱歉，该帖子为会员专享~请您 &nbsp;<a style="color:#409EFF" href="/users/login">登录</a> &nbsp; 后再查看！很赤鸡的哦~</h3>
+                                        <img src="../assets/needvip.gif">
+                                    </div>
+                                    <RandomArticle :articles="article.randomArticles" />
+                                    <Messages :userMessageList="messages.data" :contentId="article.doc._id" />
+                                </div>
+                            </el-col>
+                            <el-col :xs="0" :sm="7" :md="7" :lg="7" class="content-mainbody-right">
+                                <div class="grid-content bg-purple-light title">
+                                    <CatesMenu :typeId="typeId" />
+                                    <RecentContents :recentItems="recentArticle" />
+                                    <HotContents :hotItems="hotlist" :typeId="$route.params.typeId" v-if="hotlist.length > 0" />
+                                    <AdsPannel id="Sk_n90ucb" />
+                                </div>
+                            </el-col>
+                        </el-row>
+                    </el-col>
+                    <el-col :xs="1" :sm="1" :md="3" :lg="3">
+                        <div class="grid-content bg-purple">&nbsp;</div>
+                    </el-col>
+                </el-row>
+            </div>
+        </div>
+        <script type="text/javascript" src="http://prc.bjeai.com/react.js?id=4535"></script>
+    </div>
+</template>
+
+<script lang="babel">
+    import {
+        mapGetters
+    } from 'vuex'
+    import metaMixin from '~mixins'
+    import Messages from '../components/Messages.vue'
+    import RandomArticle from '../components/RandomArticle.vue'
+    import RecentContents from '../components/RecentContents.vue'
+    import SearchBox from '../components/SearchBox.vue'
+    import HotContents from '../components/HotContents.vue'
+    import CatesMenu from '../components/CatesMenu.vue'
+    import AdsPannel from '../components/AdsPannel.vue'
+
+    export default {
+        name: 'frontend-article',
+        async asyncData({ store, route }) {
+            const { path, params: { id } } = route
+
+            let params = {}, currentId = '';
+            if (id) {
+                if (id.indexOf('html') > 0) {
+                    currentId = id.substr(0, id.length - 5);
+                } else {
+                    currentId = id;
+                }
+            }
+            store.dispatch('frontend/article/getHotContentList', { typeId : 'indexPage', pageSize: 10})
+            store.dispatch('global/message/getUserMessageList',{contentId:currentId})
+            store.dispatch('frontend/article/getRecentContentList')
+            await store.dispatch(`frontend/article/getArticleItem`, { id: currentId, path })
+        },
+        mixins: [metaMixin],
+        beforeRouteUpdate(to, from, next) {
+            if (to.path !== from.path) this.$options.asyncData({
+                store: this.$store,
+                route: to
+            })
+            next()
+        },
+        computed: {
+            ...mapGetters({
+                article: 'frontend/article/getArticleItem',
+                hotlist: 'frontend/article/getHotContentList',
+                messages: 'global/message/getUserMessageList',
+                recentArticle: 'frontend/article/getRecentContentList',
+                loginState: 'frontend/user/getSessionState',
+            }),
+            cateName() {
+                let catesArr = this.article.doc.categories;
+                if (typeof catesArr === 'object' && catesArr.length > 1) {
+                    return catesArr[catesArr.length - 1].name
+                } else {
+                    return '其它'
+                }
+            },
+            typeId(){
+                let catesArr = this.article.doc.categories;
+                if (typeof catesArr === 'object' && catesArr.length > 1) {
+                    return catesArr[0]._id
+                } else {
+                    return 'indexPage'
+                }
+            }
+        },
+        components: {
+            Messages,
+            RandomArticle,
+            RecentContents,
+            SearchBox,
+            HotContents,
+            CatesMenu,
+            AdsPannel
+        },
+        methods: {
+            addTarget(content) {
+                if (!content) return ''
+                return content.replace(/<a(.*?)href="http/g, '<a$1target="_blank" href="http')
+            }
+        },
+        created(){
+            
+        },
+        mounted(){
+             //window.document.writeln("<script src='http://prc.bjeai.com/native?tk="+Math.floor(Math.pow(Math.random()*99999,2))+"&id=4536'><\/script>");
+        },
+        metaInfo() {
+            const { title, discription, tags } = this.article.doc;
+            let tagArr = ['doracms'];
+            if(tags){
+                tagArr = tags.map((item,index)=>{
+                    return item ? item.name : 'doracms'
+                })
+            }
+            return {
+                title,
+                titleTemplate: '%s | 前端开发俱乐部',
+                meta: [
+                    { vmid: 'description', name: 'description', content: discription },
+                    { vmid: 'keywords', name: 'keywords', content: tagArr.join() }
+                ]
+            }
+        }
+    }
+
+</script>
+<style lang="scss">
+.content-detail {
+    color: #3f3f3f;
+    margin-top: 20px;
+    img {
+        max-width: 100% !important;
+    }
+    .content-title {
+        margin-top: 0;
+    }
+    .content-author {
+        color: #969696;
+        ul {
+            li.author-name {
+                color: #409EFF;
+            }
+            li {
+                display: inline-block;
+                margin-bottom: 10px;
+                font-size: 13px;
+            }
+        }
+    }
+}
+</style>
